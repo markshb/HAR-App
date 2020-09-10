c2058f5c0f547dd12d54285529c3ce81c13248af
markshb <marcosgilguallar@gmail.com>
2020-09-11 00:55:19 +0200

    First commit

---
+++ HARApp/app/src/main/java/com/example/har_app/Classifier.java
@@ -0,0 +1,41 @@
+package com.example.har_app;
+
+import android.content.Context;
+
+import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
+
+public class Classifier {
+
+    static {
+        System.loadLibrary("tensorflow_inference");
+    }
+
+    private TensorFlowInferenceInterface inferenceInterface;
+    private static final String MODEL_FILE = "file:///android_asset/frozen_HAR.pb";
+    // Name of the input node of the LSTM
+    private static final String INPUT_NODE = "LSTM_1_input";
+    // Name of the output node of the LSTM
+    private static final String[] OUTPUT_NODES = {"Dense_2/Softmax"};
+    private static final String OUTPUT_NODE = "Dense_2/Softmax";
+    // Size of the input data
+    private static final long[] INPUT_SIZE = {1, 100, 12};
+    // Six classes to classify
+    private static final int OUTPUT_SIZE = 6;
+
+    public Classifier(final Context context) {
+        inferenceInterface = new TensorFlowInferenceInterface(context.getAssets(), MODEL_FILE);
+    }
+
+    // Feeds  the imported model with data from mobile sensors. Returns the activity that is being done.
+    public float[] predictProbabilities(float[] data) {
+
+        float[] result = new float[OUTPUT_SIZE];
+        inferenceInterface.feed(INPUT_NODE, data, INPUT_SIZE);
+        inferenceInterface.run(OUTPUT_NODES);
+        inferenceInterface.fetch(OUTPUT_NODE, result);
+
+        return result;
+    }
+
+
+}
---
+++ HARApp/app/src/main/java/com/example/har_app/MainActivity.java
@@ -0,0 +1,302 @@
+package com.example.har_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener, TextToSpeech.OnInitListener {
+
+    private static final int N_SAMPLES = 100;
+    private static int prevIdx = -1;
+
+    private static List<Float> ax;
+    private static List<Float> ay;
+    private static List<Float> az;
+
+    private static List<Float> lx;
+    private static List<Float> ly;
+    private static List<Float> lz;
+
+    private static List<Float> gx;
+    private static List<Float> gy;
+    private static List<Float> gz;
+
+    private static List<Float> ma;
+    private static List<Float> ml;
+    private static List<Float> mg;
+
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private Sensor mGyroscope;
+    private Sensor mLinearAcceleration;
+
+
+    private TextView downstairsTextView;
+    private TextView joggingTextView;
+    private TextView sittingTextView;
+    private TextView standingTextView;
+    private TextView upstairsTextView;
+    private TextView walkingTextView;
+    private TextView currentActivityTextView;
+
+    private TableRow downstairsTableRow;
+    private TableRow joggingTableRow;
+    private TableRow sittingTableRow;
+    private TableRow standingTableRow;
+    private TableRow upstairsTableRow;
+    private TableRow walkingTableRow;
+
+    private TextToSpeech textToSpeech;
+    private float[] results;
+    private Classifier classifier;
+
+    private String[] labels = {"Downstairs", "Jogging", "Sitting", "Standing", "Upstairs", "Walking"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ax = new ArrayList<>(); ay = new ArrayList<>(); az = new ArrayList<>();
+        lx = new ArrayList<>(); ly = new ArrayList<>(); lz = new ArrayList<>();
+        gx = new ArrayList<>(); gy = new ArrayList<>(); gz = new ArrayList<>();
+        ma = new ArrayList<>(); ml = new ArrayList<>(); mg = new ArrayList<>();
+
+        downstairsTextView = findViewById(R.id.downstairs_prob);
+        joggingTextView = findViewById(R.id.jogging_prob);
+        sittingTextView = findViewById(R.id.sitting_prob);
+        standingTextView = findViewById(R.id.standing_prob);
+        upstairsTextView = findViewById(R.id.upstairs_prob);
+        walkingTextView = findViewById(R.id.walking_prob);
+        currentActivityTextView = findViewById(R.id.current_activity);
+
+        downstairsTableRow = findViewById(R.id.downstairs_row);
+        joggingTableRow = findViewById(R.id.jogging_row);
+        sittingTableRow = findViewById(R.id.sitting_row);
+        standingTableRow = findViewById(R.id.standing_row);
+        upstairsTableRow = findViewById(R.id.upstairs_row);
+        walkingTableRow = findViewById(R.id.walking_row);
+
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener(this, mAccelerometer , SensorManager.SENSOR_DELAY_FASTEST);
+
+        mLinearAcceleration = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        mSensorManager.registerListener(this, mLinearAcceleration , SensorManager.SENSOR_DELAY_FASTEST);
+
+        mGyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        mSensorManager.registerListener(this, mGyroscope , SensorManager.SENSOR_DELAY_FASTEST);
+
+        classifier = new Classifier(getApplicationContext());
+
+        textToSpeech = new TextToSpeech(this, this);
+        textToSpeech.setLanguage(Locale.US);
+    }
+
+    // Initializes the app sensor manager
+    protected void onResume() {
+        super.onResume();
+        getSensorManager().registerListener(this, getSensorManager().getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_FASTEST);
+        getSensorManager().registerListener(this, getSensorManager().getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION), SensorManager.SENSOR_DELAY_FASTEST);
+        getSensorManager().registerListener(this, getSensorManager().getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    // App closed
+    @Override
+    public void onDestroy() {
+        if (textToSpeech != null) {
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
+        mSensorManager.unregisterListener(this);
+        super.onDestroy();
+    }
+
+    // Search for the max probability and if activity has changed say it on speakers.
+    @Override
+    public void onInit(int status) {
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                if (results == null || results.length == 0) {
+                    return;
+                }
+                float max = -1;
+                int idx = -1;
+                for (int i = 0; i < results.length; i++) {
+                    if (results[i] > max) {
+                        idx = i;
+                        max = results[i];
+                    }
+                }
+
+                if(max > 0.50 && idx != prevIdx) {
+                    textToSpeech.speak(labels[idx], TextToSpeech.QUEUE_ADD, null,
+                            Integer.toString(new Random().nextInt()));
+                    prevIdx = idx;
+                }
+            }
+        }, 1000, 3000);
+    }
+
+    // When sensor changes, update the probabilities and get new data
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        activityPrediction();
+
+        Sensor sensor = event.sensor;
+        if (sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            ax.add(event.values[0]);
+            ay.add(event.values[1]);
+            az.add(event.values[2]);
+
+        } else if (sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
+            lx.add(event.values[0]);
+            ly.add(event.values[1]);
+            lz.add(event.values[2]);
+
+        } else if (sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+            gx.add(event.values[0]);
+            gy.add(event.values[1]);
+            gz.add(event.values[2]);
+
+        }
+    }
+
+    // Necessary to exist due to the implemented class
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    // Calculate probability of each activity (with classifier) and puts it on textView
+    private void activityPrediction() {
+
+        List<Float> data = new ArrayList<>();
+
+        if (ax.size() >= N_SAMPLES && ay.size() >= N_SAMPLES && az.size() >= N_SAMPLES
+                && lx.size() >= N_SAMPLES && ly.size() >= N_SAMPLES && lz.size() >= N_SAMPLES
+                && gx.size() >= N_SAMPLES && gy.size() >= N_SAMPLES && gz.size() >= N_SAMPLES
+        ) {
+            double maValue; double mgValue; double mlValue;
+
+            for( int i = 0; i < N_SAMPLES ; i++ ) {
+                maValue = Math.sqrt(Math.pow(ax.get(i), 2) + Math.pow(ay.get(i), 2) + Math.pow(az.get(i), 2));
+                mlValue = Math.sqrt(Math.pow(lx.get(i), 2) + Math.pow(ly.get(i), 2) + Math.pow(lz.get(i), 2));
+                mgValue = Math.sqrt(Math.pow(gx.get(i), 2) + Math.pow(gy.get(i), 2) + Math.pow(gz.get(i), 2));
+
+                ma.add((float)maValue);
+                ml.add((float)mlValue);
+                mg.add((float)mgValue);
+            }
+
+            data.addAll(ax.subList(0, N_SAMPLES));
+            data.addAll(ay.subList(0, N_SAMPLES));
+            data.addAll(az.subList(0, N_SAMPLES));
+
+            data.addAll(lx.subList(0, N_SAMPLES));
+            data.addAll(ly.subList(0, N_SAMPLES));
+            data.addAll(lz.subList(0, N_SAMPLES));
+
+            data.addAll(gx.subList(0, N_SAMPLES));
+            data.addAll(gy.subList(0, N_SAMPLES));
+            data.addAll(gz.subList(0, N_SAMPLES));
+
+            data.addAll(ma.subList(0, N_SAMPLES));
+            data.addAll(ml.subList(0, N_SAMPLES));
+            data.addAll(mg.subList(0, N_SAMPLES));
+
+            results = classifier.predictProbabilities(toFloatArray(data));
+
+            float max = -1;
+            int idx = -1;
+            for (int i = 0; i < results.length; i++) {
+                if (results[i] > max) {
+                    idx = i;
+                    max = results[i];
+                }
+            }
+
+            Log.i("Probabilities: ", Arrays.toString(results));
+            setProbabilities();
+            setCurrentActivity(idx);
+
+            data.clear();
+            ax.clear(); ay.clear(); az.clear();
+            lx.clear(); ly.clear(); lz.clear();
+            gx.clear(); gy.clear(); gz.clear();
+        }
+    }
+
+    // Writes measured probabilities in app textView
+    private void setProbabilities() {
+        downstairsTextView.setText(Float.toString(round(results[0], 2)));
+        joggingTextView.setText(Float.toString(round(results[1], 2)));
+        sittingTextView.setText(Float.toString(round(results[2], 2)));
+        standingTextView.setText(Float.toString(round(results[3], 2)));
+        upstairsTextView.setText(Float.toString(round(results[4], 2)));
+        walkingTextView.setText(Float.toString(round(results[5], 2)));
+    }
+
+    // Set current activity in App View
+    private void setCurrentActivity(int idx) {
+        if (idx == 0)
+            currentActivityTextView.setText("DOWNSTAIRS");
+        else if (idx == 1)
+            currentActivityTextView.setText("JOGGING");
+        else if (idx == 2)
+            currentActivityTextView.setText("SITTING");
+        else if (idx == 3)
+            currentActivityTextView.setText("STANDING");
+        else if (idx == 4)
+            currentActivityTextView.setText("UPSTAIRS");
+        else if (idx == 5)
+            currentActivityTextView.setText("WALKING");
+        else
+            currentActivityTextView.setText("UNDEFINED");
+    }
+
+    // Looks for null values in float array
+    private float[] toFloatArray(List<Float> list) {
+        int i = 0;
+        float[] array = new float[list.size()];
+
+        for (Float f : list) {
+            array[i++] = (f != null ? f : Float.NaN);
+        }
+        return array;
+    }
+
+    // Round a float with input decimal places
+    private static float round(float d, int decimalPlace) {
+        BigDecimal bd = new BigDecimal(Float.toString(d));
+        bd = bd.setScale(decimalPlace, BigDecimal.ROUND_HALF_UP);
+        return bd.floatValue();
+    }
+
+    // Returns sensor manager
+    private SensorManager getSensorManager() {
+        return (SensorManager) getSystemService(SENSOR_SERVICE);
+    }
+}
